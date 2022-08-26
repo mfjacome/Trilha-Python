d575d62efb460ec47e88bc64c2b4019d70d78071
Matheus Jácome <95725545+mfjacome@users.noreply.github.com>
2022-08-25 22:54:47 -0300

    Inserindo a apresentação primordial, o famoso: Hello World!

---
+++ ola_mundo.py
@@ -0,0 +1,14 @@
+#comando básico para mostrar resultados ao usuário:
+print("Olá, mundo!")
+print('Apresentando Stings em Py')
+
+#Vale ressaltar que ao apresentar Strings é necessário utilização de aspas: simples('') ou duplas("").
+#Para tipo de dados numericos não tem a necissidade de inserir as aspas:
+print(2)
+print(30)
+print(2.00)
+print(40.50)
+
+#tipos lógicos tem as primeiras letra maiuscula: 
+print(True) #verdadeiro
+print(False) #falso
