a0aeccaeb016bcde121182ffd266e0abcf387c50
Matheus Jácome <95725545+mfjacome@users.noreply.github.com>
2022-08-25 23:14:09 -0300

    Adicionando alguns Operadores Aritmeticos

---
+++ op_aritmeticos.py
@@ -0,0 +1,20 @@
+#Adição
+print(1 + 1)
+
+#Divisão
+print(2/2)
+#Divisão (inteiros)
+print(5//2) #Retorna apenas o primeiro valor antes do ponto/vírgula
+#Resto da divisão
+print(5 % 2)
+
+#Subtração
+print(4-2)
+
+#Multiplicação
+print(2 * 2)
+
+#Exponênciação
+print(2 ** 2)
+
+#Obs: Operadores aritmeticos retornam um resultado numérico.
