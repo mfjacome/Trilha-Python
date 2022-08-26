cbb8754aa6eadf91fab884af9e7d3e974b59e657
Matheus Jácome <95725545+mfjacome@users.noreply.github.com>
2022-08-25 23:15:44 -0300

    Adicionando alguns Operadores Relacionais.

---
+++ op_relacionais.py
@@ -0,0 +1,13 @@
+#Obs: Operadores relacionais retornam em seu resultado um valor lógico: sim ou não, verdedeiro ou falso.
+num1 = 2
+num2 = 4
+maiorIgual = num1 >= num2
+print(maiorIgual)
+
+print("--------------------------")
+
+maior = num1 > num2
+
+print(maior)
+
+print("--------------------------")
