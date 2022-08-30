2e8592d7e8346142dfefb817118ae0c5f22f2462
Matheus Jácome <95725545+mfjacome@users.noreply.github.com>
2022-08-30 13:45:29 -0300

    Calculando: área de um círculo.

---
+++ calculo_area_circulo.py
@@ -0,0 +1,15 @@
+raio = input()
+pi = 3.14159
+
+area= round(float(pi) * float(raio) ** 2,4)
+
+print(f"A= {area}")
+
+
+raio = float(input())
+
+pi = 3.14159
+
+area= float(pi) * float(raio) ** 2
+
+print("A=%.4f"%area)
